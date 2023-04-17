a4733bf9c8e05310dca322df16a95bea95b76bc2
prokke <rybin.pave@yandex.ru>
2023-04-17 11:35:37 +0500

    add xml http request

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script defer src="script.js"></script>
     <title>клиент серверное приложение</title>
 </head>
 <body>
 
 
-<form action="insert-student.php" method="POST">
+<form id="form-insert-students">
     <input type="text" name="fname" id="fname" placeholder="Введите имя" required><br>
     <input type="text" name="lname" id="lname" placeholder="Введите фамилию" required><br>
     <input type="number" name="age" id="age" placeholder="Введите возраст" required><br>
---
+++ script.js
@@ -0,0 +1,10 @@
+const formInsert = document.getElementById("form-insert-students");
+formInsert.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    let formData = new FormData(formInsert);
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST","insert-student.php");
+    xhr.send(formData);
+
+    xhr.onload = ()=>{console.log()};
+});
